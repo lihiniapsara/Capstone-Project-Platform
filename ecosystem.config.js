f047b4ed8922cc9f7298f3397c5d0d4aef1fa888
Lihini Apsara <apsaralihini11@gmail.com>
2026-03-23 01:59:56 +0530

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,19 +4,19 @@ module.exports = {
     {
       name: "config-server",
       script: "java",
-      args: "-jar ./config-server/target/Config-Server-0.0.1.jar",
+      args: "-jar ./config-server/target/Config-Server-1.0.0.jar",
       log_file: "./logs/config-server.log",
     },
     {
       name: "service-registry",
       script: "java",
-      args: "-jar ./service-registry/target/Service-Registry-0.0.1.jar",
+      args: "-jar ./service-registry/target/Service-Registry-1.0.0.jar",
       log_file: "./logs/service-registry.log",
     },
     {
       name: "api-gateway",
       script: "java",
-      args: "-jar ./api-gateway/target/Api-Gateway-0.0.1.jar",
+      args: "-jar ./api-gateway/target/Api-Gateway-1.0.0.jar",
       log_file: "./logs/api-gateway.log",
     }
   ]
