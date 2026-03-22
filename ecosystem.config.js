ddef97d7bf747506a34a5b0767fac0842fae0d53
Lihini Apsara <apsaralihini11@gmail.com>
2026-03-23 01:04:27 +0530

    feat: add ecosystem configuration for application deployment

---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  apps: [
+    {
+      name: "config-server",
+      script: "java",
+      args: "-jar ./config-server/target/Config-Server-0.0.1.jar",
+      log_file: "./logs/config-server.log",
+    },
+    {
+      name: "service-registry",
+      script: "java",
+      args: "-jar ./service-registry/target/Service-Registry-0.0.1.jar",
+      log_file: "./logs/service-registry.log",
+    },
+    {
+      name: "api-gateway",
+      script: "java",
+      args: "-jar ./api-gateway/target/Api-Gateway-0.0.1.jar",
+      log_file: "./logs/api-gateway.log",
+    }
+  ]
+}
